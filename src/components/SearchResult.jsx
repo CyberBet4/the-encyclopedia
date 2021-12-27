40dc3866572558ca14665faf07b54b7ce955e780
CyberBet4 <triumphanazia@gmail.com>
2021-12-27 13:43:40 +0100

    Fixed #16

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -8,68 +8,81 @@ import axios from 'axios'
 const SearchResult = ({ keyword }) => {
 
     const [ stat, setStat ] = useState({})
+    const [ searchInit, setsearchInit ] = useState(false)
     
-    // get Error function
-    const getErrorStatus = (err) => {
-        err === 0 ? displayError() : <></>
-        console.log(err);
-    }
+    useEffect(() => {
 
-     useEffect(() => {
-        
+        // get Error function
+        const getErrorStatus = (err) => {
+            err === 0 ? displayError() : <></>
+            console.log(err);
+        } 
+
         //api from News Api  
-        const fetchData = async () => {
+    
+        const FetchData = async () => {
             try{
                 const res = await axios.get(`https://newsapi.org/v2/everything?q=${keyword}&apiKey=375033e55f524d279c9021abdf94f0ed`) //6515b9f031004066b18e05b72c87aee1
 
-                        const datas = res.data.articles
+                const datas = res.data.articles
 
-                        setStat({
-                            errorMessage : '',
-                            results : datas,
-                            status : true 
-                        })
-                } catch(err){
+                setStat({
+                    errorMessage : '',
+                    results : datas,
+                    status : true 
+                })
+                
+                // console.log(searchInit);
+            } catch(err){
                     console.log(err);
                     getErrorStatus(err.request.status);
                     // Hook doesn't work here, it'll just be doing unlimited rendering
-                }
+            }
         }
-        fetchData(); //run axios function
-    })
-     
+
+        //run axios function when keyword is not empty
+        if (keyword !== '') {
+            FetchData();
+            setsearchInit(true) 
+        }   
+    }, [keyword])
+
+
     // display error
     const displayError = () => <p className="alert text-center alert-danger">Sorry! An error occurred</p>
     
     const getDatas = () => {
         // displays the data received
         return (
-        
-        stat.results.map(data => {
-            return <ResultCards key={data.url}
-                image={data.urlToImage} 
-                title={data.title} 
-                link={data.url}
-                desc={`${data.description.slice(0, 200)}...`}
-                date={`${data.publishedAt.slice(0, 10)}`}
-                />
-        })
+            stat.results.map(data => {
+                return <ResultCards key={data.url}
+                    image={data.urlToImage} 
+                    title={data.title} 
+                    link={data.url}
+                    desc={`${data.description.slice(0, 200)}...`}
+                    date={`${data.publishedAt.slice(0, 10)}`}
+                    />
+            })
         )
+    }
+
+    const displayResult = () => {
+        console.log(searchInit);
+        if(!searchInit){
+            return <>Nothing Yet!</>
+        }
+        else if(stat.status && searchInit){
+            return getDatas()
+         }else{
+            return displayError()
+         }        
     }
 
     return (
         
         <div >
             <div style={{width : 450}}>
-                
-                {stat.status ? 
-                // if status of the search is true
-                getDatas()
-                : 
-                // if status of the search is false   
-                displayError()
-                // No Results found yet :)  
-                }
+                {displayResult()}
             </div>
             
             <div className="d-flex justify-content-center">
